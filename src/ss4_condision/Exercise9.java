cebde5be78eadcf68e3757bfe12becd459551c3c
NhatKha2608 <nnk26082003@gmail.com>
2025-03-07 18:01:25 +0700

    Update Exercise9.java

--- src/ss4_condision/Exercise9.java
+++ src/ss4_condision/Exercise9.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Exercise9 {
     public static void main(String[] args) {
-        //Nhập vào 1 ký tự là chữ cái. Nếu nhập sai thì báo thông báođ ã nhập sai.
+        //Nhập vào 1 ký tự là chữ cái. Nếu nhập sai thì báo thông báo đã nhập sai.
         // Nếu đúng thì đi kiểm tra nếuđó đang là chữ cái thường thì biến nó thành chữ cái hoa,
         // nếu nó là chữ cái hoa thì biến nó thành chữ cái thường.
         // Gợiý: Ký tự chữ cái thường và hoa có mã ASCII cách nhau 32 đơn vị. Vd: 'a' là 97, 'A' là 65
